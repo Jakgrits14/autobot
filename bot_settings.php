4a29caf50e9f64971dbc798ad5e89319f88f9f11
Jakgrits14 <37709056+Jakgrits14@users.noreply.github.com>
2018-03-27 12:08:31 +0700

    Update bot_settings.php

--- bot_settings.php
+++ bot_settings.php
@@ -2,5 +2,5 @@
 /// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
 define('LINE_MESSAGE_CHANNEL_ID','1570102577');
 define('LINE_MESSAGE_CHANNEL_SECRET','91978f842fb92cd207d514d605509b35');
-define('LINE_MESSAGE_ACCESS_TOKEN','aRVe2ljy5XpffVHF+u5fjTmPOyYDDNca1/9ebxe+q8D5fJbn+kUgx/G6tYEn8p0h3BwctdLUEIjge+Labpcohi+gb8YaHbJFuAx2Jc2XTag3gXaIWSPgfD2Jk414h3blEjraXDTFvYmew4nWxCdIOQdB04t89/1O/w1cDnyilFU=');
+define('LINE_MESSAGE_ACCESS_TOKEN','fMcjIQwxtTaSpnukO+x2A9tWXzq5pYWL73lfItfkrYSd2G0+i04XbiHa7wdlBuOG3BwctdLUEIjge+Labpcohi+gb8YaHbJFuAx2Jc2XTajdUHOzg01TCZDko8TPrVZ6mBiV06JlmbYaWD2dsptItAdB04t89/1O/w1cDnyilFU=');
 ?>
